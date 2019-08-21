8b2b5744299634803b718a2dc9094b7b481e2f55
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-08-20 23:22:31 -0400

    clone

--- init.zsh
+++ init.zsh
@@ -10,13 +10,24 @@ p6df::modules::github::external::brew() {
   brew install hub
 }
 
-p6df::modules::github::home::symlink() {
+p6df::modules::github::langs() {
 
-  # XXX: de XDG
-  true;
+  local org
+  for org in $(cat $P6_DFZ_SRC_P6M7G8_DIR/p6github/conf/orgs); do
+    p6_GLOBAL_github_api_org_repos_clone "$org"
+  done
+
 }
+
+p6df::modules::github::home::symlink() { }
 
 p6df::modules::github::init() {
 
     eval "$(hub alias -s)"
+}
+
+p6_GLOBAL_github_api_org_repos_clone() {
+  local org="$1"
+
+  p6_github_api_org_repos_clone "https://github.com" "https://api.github.com" "$org" "$P6_DFZ_SRC_DIR" "" 8
 }
