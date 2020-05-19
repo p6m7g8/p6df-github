cd72573626883856fe8e72c2e977acbec899e273
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-05-19 18:47:57 -0400

    feature(gh): transition from hub to gh

--- init.zsh
+++ init.zsh
@@ -1,13 +1,16 @@
 p6df::modules::github::version() { echo "0.0.1" }
-p6df::modules::github::deps()    { 
+p6df::modules::github::deps()    {
 	ModuleDeps=(
 		p6m7g8/p6github
 	)
 }
 
-p6df::modules::github::external::brew() { 
+p6df::modules::github::external::brew() {
 
+  p6__deprecated "brew install bub"
   brew install hub
+
+  brew install github/gh/gh
 }
 
 p6df::modules::github::langs() {
@@ -16,13 +19,13 @@ p6df::modules::github::langs() {
   for org in $(cat $P6_DFZ_SRC_P6M7G8_DIR/p6github/conf/orgs); do
     p6_GLOBAL_github_api_org_repos_clone "$org"
   done
-
 }
 
 p6df::modules::github::home::symlink() { }
 
 p6df::modules::github::init() {
 
+    p6__deprecated "hub alias -s"
     eval "$(hub alias -s)"
 }
 
